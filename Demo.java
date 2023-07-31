61e6fd6740202065117fb125fa5fdae13f7bc323
Kirtidas S. There <kirtidasthere@gmail.com>
2023-07-31 17:57:47 +0530

    Demo.java modified added Demo1

--- Demo.java
+++ Demo.java
@@ -7,3 +7,13 @@ System.out.println(args[0]);
 System.out.println(args[1]);
 }
 }
+
+class Demo1
+{
+public static void main(String args[])
+{
+System.out.println("Hello All");
+System.out.println(args[0]);
+System.out.println(args[1]);
+}
+}
